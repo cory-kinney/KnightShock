cd8e45c96c21fd839bf63b9408941614f5f88392
Unknown <ckinneydev@gmail.com>
2020-11-10 19:04:20 -0500

    Implemented PyShock

--- knight_shock.py
+++ knight_shock.py
@@ -1,221 +1,146 @@
+"""Shock tube experiment data analysis framework"""
+
 import cantera as ct
 import numpy as np
-
-R = ct.gas_constant
-
-
-def FROSH(T1, P1, u1, *, thermo, max_iterations=1000, convergence_criteria=1e-6):
-    """
-    Implementation of the FROzen SHock (FROSH) algorithm for calculating post-incident-shock and post-reflected-shock
-    conditions from initial conditions and shock velocity for a shock tube experiment
-
-    Args:
-        T1: initial temperature in driven section [K]
-        P1: initial pressure in driven section [Pa]
-        u1: incident shock velocity [m/s]
-        thermo: object for temperature- and pressure-dependent thermodynamic property calculation
-        max_iterations: maximum number of iterations for calculating region 2 and 5 temperature and pressure conditions
-        convergence_criteria: maximum change between iterations for convergence
-
-    Returns:
-        T2: temperature in the post-incident-shock region [K]
-        P2: pressure in the post-incident-shock region [Pa]
-        T5: temperature in the post-reflected-shock region [K]
-        P5: pressure in the post-reflected-shock region [Pa]
-    """
-
-    # Get relevant properties for mixture at T1 and P1
-    thermo.TP = T1, P1
-
-    gamma1 = thermo.cp_mass / thermo.cv_mass
-    a1 = (ct.gas_constant / thermo.mean_molecular_weight * gamma1 * T1) ** 0.5  # [m/s]
-    v1 = 1 / thermo.density_mass  # [m^3/kg]
-    h1 = thermo.enthalpy_mass  # [J/kg]
-
-    # Calculates ideal P2 and T2 for initial guesses
-    Ms = u1 / a1
-
-    T2 = T1 * ((gamma1 * Ms ** 2 - (gamma1 - 1) / 2) * ((gamma1 - 1) / 2 * Ms ** 2 + 1)) \
-         / (((gamma1 + 1) / 2) ** 2 * Ms ** 2)  # [K]
-    P2 = P1 * (2 * gamma1 * Ms ** 2 - (gamma1 - 1)) / (gamma1 + 1)  # [Pa]
-
-    T2_guess = T2
-    P2_guess = P2
-
-    # Iterate to find P2 and T2
-    for i in range(max_iterations):
-        thermo.TP = T2_guess, P2_guess
-
-        # Get relevant properties for the mixture at the guessed P2 and T2 values
-        v2 = 1 / thermo.density_mass  # [m^3/kg]
-        h2 = thermo.enthalpy_mass  # [J/kg]
-        cp2 = thermo.cp_mass  # [J/kg/K]
-
-        # Evaluate functions
-        f1 = (P2_guess / P1 - 1) + (u1 ** 2 / (P1 * v1)) * (v2 / v1 - 1)
-        f2 = ((h2 - h1) / (1 / 2 * u1 ** 2)) + (v2 ** 2 / v1 ** 2 - 1)
-
-        df1_dP2_T2 = (1 / P1) + (u1 ** 2 / (P1 * v1 ** 2)) * (-v2 / P2_guess)
-        df1_dT2_P2 = (u1 ** 2 / (P1 * v1 ** 2)) * (v2 / T2_guess)
-        df2_dP2_T2 = (2 * v2 / v1 ** 2) * (-v2 / P2_guess)
-        df2_dT2_P2 = (2 / u1 ** 2) * cp2 + (2 * v2 / v1 ** 2) * (v2 / T2_guess)
-
-        T2, P2 = tuple((np.array([[T2_guess], [P2_guess]])
-                        - np.matmul(np.linalg.inv(np.array([[df1_dT2_P2, df1_dP2_T2], [df2_dT2_P2, df2_dP2_T2]])),
-                                    np.array([[f1], [f2]]))).T[0])
-
-        # Break the loop when convergence criteria is met
-        if abs(T2 - T2_guess) / T2_guess < convergence_criteria and \
-                abs(P2 - P2_guess) / P2_guess < convergence_criteria:
-            break
-
-        # Update the P2 and T2 guesses
-        T2_guess = T2
-        P2_guess = P2
-
-        # Raise an error if the maximum number of iterations has been reached
-        if i == max_iterations - 1:
-            raise RuntimeError("Convergence criteria not met within the maximum number of iterations")
-
-    # Get relevant properties for mixture at T2 and P2
-    thermo.TP = T2, P2
-
-    gamma2 = thermo.cp_mass / thermo.cv_mass
-    v2 = 1 / thermo.density_mass  # [m^3/kg]
-    h2 = thermo.enthalpy_mass  # [J/kg]
-
-    # Calculate u2 using continuity
-    u2 = u1 * v2 / v1  # [m/s]
-
-    # Calculates ideal P5 and T5 for initial guesses
-    P5 = P2 * ((gamma2 + 1) / (gamma2 - 1) + 2 - P1 / P2) / (1 + (gamma2 + 1) / (gamma2 - 1) * P1 / P2)  # [Pa]
-    T5 = T2 * (P5 / P2) * ((gamma2 + 1) / (gamma2 - 1) + P5 / P2) / (1 + (gamma2 + 1) / (gamma2 - 1) * P5 / P2)  # [K]
-
-    T5_guess = T5
-    P5_guess = P5
-
-    # Iterate to find P5 and T5
-    for i in range(max_iterations):
-        # Get relevant properties for the mixture at the guessed P5 and T5 values
-        thermo.TP = T5_guess, P5_guess
-
-        v5 = 1 / thermo.density_mass  # [m^3/kg]
-        h5 = thermo.enthalpy_mass  # [J/kg]
-        cp5 = thermo.cp_mass  # [J/kg/K]
-
-        # Evaluate functions
-        f3 = (P5_guess / P2 - 1) + ((u1 - u2) ** 2 / (P2 * (v5 - v2)))
-        f4 = ((h5 - h2) / (1 / 2 * (u1 - u2) ** 2)) + ((v5 + v2) / (v5 - v2))
-
-        df3_dP5_T5 = (1 / P2) + (-(u1 - u2) ** 2 / (P2 * (v5 - v2) ** 2)) * (-v5 / P5_guess)
-        df3_dT5_P5 = (-(u1 - u2) ** 2 / (P2 * (v5 - v2) ** 2)) * (v5 / T5_guess)
-        df4_dP5_T5 = (-2 * v2 / (v5 - v2) ** 2) * (-v5 / P5_guess)
-        df4_dT5_P5 = (1 / (1 / 2 * (u1 - u2) ** 2)) * cp5 + (-2 * v2 / (v5 - v2) ** 2) * (v5 / T5_guess)
-
-        T5, P5 = tuple((np.array([[T5_guess], [P5_guess]])
-                        - np.matmul(np.linalg.inv(np.array([[df3_dT5_P5, df3_dP5_T5], [df4_dT5_P5, df4_dP5_T5]])),
-                                    np.array([[f3], [f4]]))).T[0])
-
-        # Break the loop when convergence criteria is met
-        if abs(T5 - T5_guess) / T5_guess < convergence_criteria and \
-                abs(P5 - P5_guess) / P5_guess < convergence_criteria:
-            break
-
-        # Update the P5 and T5 guesses
-        T5_guess = T5
-        P5_guess = P5
-
-        # Raise an error if the maximum number of iterations has been reached
-        if i == max_iterations - 1:
-            raise RuntimeError("Convergence criteria not met within the maximum number of iterations")
-
-    return T2, P2, T5, P5
+import pyshock
 
 
 class Experiment:
-    """
-    Regions:
-        1 - initial conditions driven section
-        2 - post-incident-shock conditions driven section
-        3 - expanded conditions driver section
-        4 - initial conditions driver section
-        5 - post-reflected-shock driven section
+    """Base class for shock tube experiments
+
+    Attributes
+    ----------
+    T1 : float
+        initial temperature [K] of the driven gas
+    P1 : float
+        initial pressure [Pa] of the driven gas
+    T4 : float
+        initial temperature [K] of the driver gas
+    P4 : float
+        initial pressure [Pa] of the driver gas
+    driven_mixture : str
+        Mixture composition of driven gas as comma-separated species: mole fraction pairs
+    driver_mixture : str
+        Mixture composition of driver gas as comma-separated species: mole fraction pairs
+    T2 : float
+        temperature [K] of the driven gas after the incident shock wave
+    P2 : float
+        pressure [Pa] of the driven gas after the incident shock wave
+    T5 : float
+        temperature [K] of the driver gas after the reflected shock wave
+    P5 : float
+        pressure [Pa] of the driver gas after the reflected shock wave
+
     """
 
-    def __init__(self, mechanism):
-        self.T1 = None  # [K]
-        self.T4 = None  # [K]
-        self.P1 = None  # [Pa]
-        self.P4 = None  # [Pa]
+    def __init__(self):
+        self.T1 = None
+        self.P1 = None
+        self.T4 = None
+        self.P4 = None
 
-        self.driver_mixture = None
         self.driven_mixture = None
+        self.driver_mixture = None
 
-        self._u = None  # [m/s]
+        self.T2 = None
+        self.P2 = None
+        self.T5 = None
+        self.P5 = None
 
-        self.T2 = None  # [K]
-        self.P2 = None  # [Pa]
-        self.T5 = None  # [K]
-        self.P5 = None  # [Pa]
+        self._thermo = None
 
-        self.thermo = ct.Solution(mechanism)
+    @property
+    def thermo(self):
+        """
+        `cantera.ThermoPhase` object for temperature- and pressure-dependent thermodynamic property calculations
+
+        .. Attention::
+           Temperature, pressure, and mixture composition (`TPX`) of `cantera.ThermoPhase` object must be updated before
+           accessing thermodynamic properties
+
+        """
+        return self._thermo
+
+    @thermo.setter
+    def thermo(self, value):
+        if isinstance(value, ct.ThermoPhase):
+            self._thermo = value
+        elif isinstance(value, str):
+            try:
+                self._thermo = ct.ThermoPhase(value)
+            except ct.CanteraError:
+                raise ValueError("Invalid mechanism file")
+        else:
+            raise TypeError("Input must be a Cantera ThermoPhase object or file path to a valid mechanism file")
 
     @property
     def gamma1(self):
+        """Specific heat ratio of driven gas at initial conditions"""
         self.thermo.TPX = self.T1, self.P1, self.driven_mixture
         return self.thermo.cp / self.thermo.cv
 
     @property
     def gamma4(self):
+        """Specific heat ratio of driver gas at initial conditions"""
         self.thermo.TPX = self.T4, self.P4, self.driver_mixture
         return self.thermo.cp / self.thermo.cv
 
     @property
     def a1(self):
+        """Speed of sound [m/s] in driven gas at initial conditions"""
         self.thermo.TPX = self.T1, self.P1, self.driven_mixture
         return (self.thermo.cp / self.thermo.cv * ct.gas_constant * self.T1 / self.thermo.mean_molecular_weight) ** 0.5
 
     @property
     def a4(self):
+        """Speed of sound [m/s] in driver gas at initial conditions"""
         self.thermo.TPX = self.T4, self.P4, self.driver_mixture
         return (self.thermo.cp / self.thermo.cv * ct.gas_constant * self.T4 / self.thermo.mean_molecular_weight) ** 0.5
 
-    @property
-    def u(self):
-        return self._u
+    def calculate_shock_conditions(self, u):
+        """Calculates `T2`, `P2`, `T5`, and `P5` for the experiment using `pyshock.FROSH`
 
-    @u.setter
-    def u(self, value):
-        if value < 0:
-            raise ValueError
+        Parameters
+        ----------
+        u : float
+            incident shock wave velocity [m/s]
 
-        self._u = value
-        self.T2, self.P2, self.T5, self.P5 = FROSH(self.T1, self.P1, self.u, thermo=self.thermo)
+        """
 
-    @staticmethod
-    def get_shock_speed(x, t):
-        n = len(x)
+        self.thermo.X = self.driven_mixture
+        self.T2, self.P2, self.T5, self.P5 = pyshock.FROSH(self.T1, self.P1, u, thermo=self.thermo)
 
-        if len(t) == n:
-            t = np.diff(t)
 
-        if len(t) != n - 1:
-            raise ValueError
+def calculate_shock_velocity(x, dt):
+    """Computes the least-squares linear fit of average shock velocities over intervals
 
-        x_midpoint = np.empty(n - 1)
-        u_avg = np.empty(n - 1)
+    Parameters
+    ----------
+    x : numpy.ndarray
+        positions relative to end wall
+    dt : numpy.ndarray
+        differences in shock wave arrival times over intervals
 
-        for i in range(n - 1):
-            x_midpoint[i] = (x[i] + x[i + 1]) / 2
-            u_avg[i] = (x[i] - x[i + 1]) / t[i]
+    Returns
+    -------
+    u : float
+        shock velocity extrapolated to end wall
+    attenuation : float
+        deceleration of the shock relative to end wall velocity
+    r2 : float
+        coefficient of determination of the linear fit
 
-        A = np.vstack([x_midpoint, np.ones(n - 1)]).T
-        model, residual = np.linalg.lstsq(A, u_avg, rcond=None)[:2]
-        r2 = 1 - residual / (u_avg.size * u_avg.var())
+    """
 
-        u = model[1]
-        attenuation = model[0] / model[1]
+    x_midpoint = (x[1:] + x[:-1]) / 2
+    u_avg = np.abs(np.diff(x)) / dt
 
-        return u, attenuation, r2
+    A = np.vstack([x_midpoint, np.ones(len(x_midpoint))]).T
+    model, residual = np.linalg.lstsq(A, u_avg, rcond=None)[:2]
+    r2 = (1 - residual / (u_avg.size * u_avg.var()))[0]
 
+    u = model[1]
+    attenuation = model[0] / model[1]
+
+    return u, attenuation, r2
 
