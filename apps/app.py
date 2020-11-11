127628fb10680b2863cfcc3e442d82f210e55862
Unknown <ckinneydev@gmail.com>
2020-11-11 10:30:42 -0500

    Starting Dash apps

---
+++ apps/app.py
@@ -0,0 +1,8 @@
+import dash
+import dash_bootstrap_components as dbc
+
+
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+server = app.server
+
+app.config.suppress_callback_exceptions = True
