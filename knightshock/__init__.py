61b512356e6d6906676b06125f445cfdfc8a088b
Cory Kinney <ckinneydev@gmail.com>
2020-12-15 10:19:19 -0500

    Test

--- knightshock/__init__.py
+++ knightshock/__init__.py
@@ -23,202 +23,272 @@ import cantera as ct
 import numpy as np
 
 
+# Optional unit conversion support
+try:
+    import pint
+    units = pint.UnitRegistry()
+except ImportError:
+    pint = None
+    units = None
+
+
+class ShockVelocityError(Exception):
+    """Raised when shock velocity is invalid"""
+    pass
+
+
 class ShockTubeState:
-    """
+    """Base class defining the state of the shock tube with basic validation of parameter values"""
 
-    Attributes
-    ----------
-    driven_mixture : str
-        mixture composition of driven gas as comma-separated species and mole fraction pairs
-    driver_mixture : str
-        mixture composition of driver gas as comma-separated species and mole fraction pairs
-    T1 : float
-        initial temperature of the driven gas
-    P1 : float
-        initial pressure of the driven gas
-    T4 : float
-        initial temperature of the driver gas
-    P4 : float
-        initial pressure of the driver gas
-    T2 : float
-        temperature of the driven gas after the incident shock
-    P2 : float
-        pressure of the driven gas after the incident shock
-    T5 : float
-        temperature of the driver gas after the reflected shock
-    P5 : float
-        pressure of the driver gas after the reflected shock
-    u : float
-        velocity of incident shock
-    M : float
-        Mach number of incident shock
-    area_ratio : float
-        ratio of driver area to driven area
+    def __init__(self, solution):
+        if not isinstance(solution, ct.Solution):
+            raise TypeError
 
-    Notes
-    -----
-    Temperature and pressure must be in units of K and Pa, respectively, for thermodynamic property calculations.
+        self._solution = solution
+        self._driven_mixture = None
+        self._driver_mixture = None
 
-    """
+        self._T1 = None
+        self._P1 = None
+        self._T2 = None
+        self._P2 = None
+        self._T4 = None
+        self._P4 = None
+        self._T5 = None
+        self._P5 = None
 
-    def __init__(self, thermo):
-        """
-        Parameters
-        ----------
-        thermo:
-            object for temperature- and pressure-dependent thermodynamic property calculations
+        self._M = None
+        self._area_ratio = None
 
-        """
+    @property
+    def driven_mixture(self):
+        """"""
+        return self._driven_mixture
 
-        if not isinstance(thermo, ct.ThermoPhase):
-            raise TypeError("Thermo must be instance of cantera.ThermoPhase")
+    @driven_mixture.setter
+    def driven_mixture(self, value):
+        self._solution.X = value  # Attempts to set mixture property for Cantera to validate input
+        self._driven_mixture = value
 
-        self.thermo = thermo
-        self.driven_mixture = None
-        self.driver_mixture = None
+    @property
+    def driver_mixture(self):
+        """"""
+        return self._driver_mixture
 
-        self.T1 = None
-        self.P1 = None
-        self.T2 = None
-        self.P2 = None
-        self.T4 = None
-        self.P4 = None
-        self.T5 = None
-        self.P5 = None
+    @driver_mixture.setter
+    def driver_mixture(self, value):
+        self._solution.X = value  # Attempts to set mixture property for Cantera to validate input
+        self._driver_mixture = value
 
-        self.u = None
-        self.M = None
+    @property
+    def T1(self):
+        """Initial driven state temperature [K]"""
+        return self._T1
 
-        self.area_ratio = 1
+    @T1.setter
+    def T1(self, value):
+        if not value > self._solution.min_temp:
+            raise ValueError("Temperature must be greater than zero")
+        self._T1 = value
+
+    @property
+    def T2(self):
+        """Post-incident-shock driven state temperature [K]"""
+        return self._T2
+
+    @T2.setter
+    def T2(self, value):
+        if not value > 0:
+            raise ValueError("Temperature must be greater than zero")
+        self._T2 = value
+
+    @property
+    def T4(self):
+        """Initial driver state temperature [K]"""
+        return self._T4
+
+    @T4.setter
+    def T4(self, value):
+        if not value > 0:
+            raise ValueError("Temperature must be greater than zero")
+        self._T4 = value
+
+    @property
+    def T5(self):
+        """Post-reflected-shock driven state temperature [K]"""
+        return self._T5
+
+    @T5.setter
+    def T5(self, value):
+        if not value > 0:
+            raise ValueError("Temperature must be greater than zero")
+        self._T5 = value
+
+    @property
+    def P1(self):
+        """"""
+        return self._P1
+
+    @P1.setter
+    def P1(self, value):
+        if not value > 0:
+            raise ValueError("Pressure must be greater than zero")
+        self._P1 = value
+
+    @property
+    def P2(self):
+        """"""
+        return self._P2
+
+    @P2.setter
+    def P2(self, value):
+        if not value > 0:
+            raise ValueError("Pressure must be greater than zero")
+        self._P2 = value
+
+    @property
+    def P4(self):
+        """"""
+        return self._P4
+
+    @P4.setter
+    def P4(self, value):
+        if not value > 0:
+            raise ValueError("Pressure must be greater than zero")
+        self._P4 = value
+
+    @property
+    def P5(self):
+        """"""
+        return self._P5
+
+    @P5.setter
+    def P5(self, value):
+        if not value > 0:
+            raise ValueError("Pressure must be greater than zero")
+        self._P5 = value
+
+    @property
+    def M(self):
+        """Mach number of incident shock wave"""
+        return self._M
+
+    @M.setter
+    def M(self, value):
+        if not value > 1:
+            raise ValueError("Mach number must be greater than one")
+        self._M = value
+
+    @property
+    def u(self):
+        """Velocity of incident shock wave [m/s]"""
+        return self._M * self.a1
+
+    @property
+    def area_ratio(self):
+        """Ratio of area of driver to driven"""
+        return self._area_ratio
+
+    @area_ratio.setter
+    def area_ratio(self, value):
+        if value < 1:
+            raise ValueError("Area ratio must be greater than or equal to one")
 
     @property
     def MW1(self):
-        """Mean molecular weight of driven gas at initial conditions"""
-        self.thermo.TPX = self.T1, self.P1, self.driven_mixture
-        return self.thermo.mean_molecular_weight
+        """Initial driven mixture mean molecular weight [kg/kmol]"""
+        self._solution.TPX = self.T1, self.P1, self.driven_mixture
+        return self._solution.mean_molecular_weight
 
     @property
     def MW4(self):
-        """Mean molecular weight of driver gas at initial conditions"""
-        self.thermo.TPX = self.T4, self.P4, self.driver_mixture
-        return self.thermo.mean_molecular_weight
+        """Initial driver mixture mean molecular weight [kg/kmol]"""
+        self._solution.TPX = self.T4, self.P4, self.driver_mixture
+        return self._solution.mean_molecular_weight
 
     @property
     def gamma1(self):
         """Specific heat ratio of driven gas at initial conditions"""
-        self.thermo.TPX = self.T1, self.P1, self.driven_mixture
-        return self.thermo.cp / self.thermo.cv
+        self._solution.TPX = self.T1, self.P1, self.driven_mixture
+        return self._solution.cp / self._solution.cv
 
     @property
     def gamma4(self):
         """Specific heat ratio of driver gas at initial conditions"""
-        self.thermo.TPX = self.T4, self.P4, self.driver_mixture
-        return self.thermo.cp / self.thermo.cv
+        self._solution.TPX = self.T4, self.P4, self.driver_mixture
+        return self._solution.cp / self._solution.cv
 
     @property
     def a1(self):
-        """Speed of sound in driven gas at initial conditions"""
-        self.thermo.TPX = self.T1, self.P1, self.driven_mixture
-        return (self.thermo.cp / self.thermo.cv * ct.gas_constant * self.T1 / self.thermo.mean_molecular_weight) ** 0.5
+        """Speed of sound in driven gas at initial conditions [m/s]"""
+        self._solution.TPX = self.T1, self.P1, self.driven_mixture
+        return (self._solution.cp / self._solution.cv * ct.gas_constant / self._solution.mean_molecular_weight
+                * self.T1) ** 0.5
 
     @property
     def a4(self):
-        """Speed of sound in driver gas at initial conditions"""
-        self.thermo.TPX = self.T4, self.P4, self.driver_mixture
-        return (self.thermo.cp / self.thermo.cv * ct.gas_constant * self.T4 / self.thermo.mean_molecular_weight) ** 0.5
+        """Speed of sound in driver gas at initial conditions [m/s]"""
+        self._solution.TPX = self.T4, self.P4, self.driver_mixture
+        return (self._solution.cp / self._solution.cv * ct.gas_constant / self._solution.mean_molecular_weight
+                * self.T4) ** 0.5
 
 
 class Experiment(ShockTubeState):
     """Shock tube experiment base class that extends the `knightshock.ShockTubeState` class
-
-    Attributes
-    ----------
-    x : numpy.ndarray
-        positions relative to end wall
-    dt : numpy.ndarray
-        differences in shock wave arrival times over intervals
 
     """
 
     def __init__(self, thermo):
         super().__init__(thermo)
 
-        # Data points for shock velocity time-of-flight calculation
-        self._x = None
-        self._dt = None
+        pass
 
-        self._x_midpoint = None
-        self._u_average = None
-
-        self.attenuation = None
-        self.r2 = None
-
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self, value):
-        self._x = np.array(value)
-
-    @property
-    def dt(self):
-        return self._dt
-
-    @dt.setter
-    def dt(self, value):
-        self._dt = np.array(value)
-
-    def update_shock_velocity(self):
-        """Computes the least-squares linear fit of average shock velocities over intervals
-
-        Raises
-        ------
-        ValueError
-            `x` values are not greater than zero
-        ValueError
-            `x` values are not strictly decreasing
-
-        """
-
-        if np.any(self.x < 0):
-            raise ValueError("x values must be positive")
-        if not np.all(self.x[1:] < self.x[:-1]):
-            raise ValueError("x values must be strictly decreasing")
-
-        self._x_midpoint = (self.x[1:] + self.x[:-1]) / 2
-        self._u_average = np.abs(np.diff(self.x)) / self.dt
-
-        A = np.vstack([self._x_midpoint, np.ones(len(self._x_midpoint))]).T
-        model, residual = np.linalg.lstsq(A, self._u_average, rcond=None)[:2]
-
-        self.u = model[1]
-        self.attenuation = model[0] / model[1]
-        self.r2 = (1 - residual / (self._u_average.size * self._u_average.var()))[0]
-
-    def update_shock_conditions(self, *, constant_properties=False):
+    def calculate_shock_conditions(self, u, *, constant_properties=False):
         """
         Parameters
         ----------
         constant_properties : bool (optional)
 
         """
-        if constant_properties:
-            self.T2 = gas_dynamics.T2_ideal(self.T1, self.M, self.gamma1)
-            self.P2 = gas_dynamics.P2_ideal(self.P1, self.M, self.gamma1)
-            self.T5 = gas_dynamics.T5_ideal(self.T1, self.M, self.gamma1)
-            self.P5 = gas_dynamics.P5_ideal(self.P1, self.M, self.gamma1)
-        else:
-            self.thermo.X = self.driven_mixture
-            self.T2, self.P2, self.T5, self.P5 = \
-                gas_dynamics.shock_conditions_FROSH(self.T1, self.P1, self.M, thermo=self.thermo)
+        M = u / self.a1
+        if M < 1:
+            raise ShockVelocityError
 
-    def plot_shock_velocity(self):
-        plt.figure()
-        plt.scatter(self._x_midpoint, self._u_average, color='r')
-        plt.plot([0, self.x.max()], [self.u, self.u * (1 + self.attenuation * self.x.max())], '-k')
-        plt.xlim([0, self.x.max()])
-        plt.show()
+        if constant_properties:
+            T2 = gas_dynamics.T2_ideal(self.T1, self.M, self.gamma1)
+            P2 = gas_dynamics.P2_ideal(self.P1, self.M, self.gamma1)
+            T5 = gas_dynamics.T5_ideal(self.T1, self.M, self.gamma1)
+            P5 = gas_dynamics.P5_ideal(self.P1, self.M, self.gamma1)
+        else:
+            self._solution.X = self.driven_mixture
+            T2, P2, T5, P5 = gas_dynamics.shock_conditions_FROSH(self.T1, self.P1, self.M, thermo=self._solution)
+
+        return T2, P2, T5, P5
+
+    @staticmethod
+    def calculate_shock_velocity(x, dt):
+        """Calculates the shock velocity by time-of-flight
+
+        Parameters
+        ----------
+        x : numpy.ndarray
+            measurement positions relative to endwall
+        dt : numpy.ndarray
+            time-of-flight over interval
+
+        """
+
+        x_midpoint = (x[1:] + x[:-1]) / 2
+        u_average = np.abs(np.diff(x)) / dt
+
+        A = np.vstack([x_midpoint, np.ones(len(x_midpoint))]).T
+        model, residual = np.linalg.lstsq(A, u_average, rcond=None)[:2]
+
+        u = model[1]
+        attenuation = model[0] / model[1]
+        r2 = (1 - residual / (u_average.size * u_average.var()))[0]
+
+        return u, attenuation, r2
 
 
 class ExperimentPlan(ShockTubeState):
@@ -287,7 +357,6 @@ class ExperimentPlan(ShockTubeState):
         else:
             raise ValueError("Invalid method specified")
 
-        self.u = self.M * self.a1
         self.P2 = gas_dynamics.P2_ideal(self.P1, self.M, self.gamma1)
         self.T2 = gas_dynamics.T2_ideal(self.T1, self.M, self.gamma1)
         if method != "P5/P1":
